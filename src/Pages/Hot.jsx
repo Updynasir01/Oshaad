d497c9acd5a028ab0eb03ebf624f8a97161090d1
AB- nazER <updynasir001@gmail.com>
2025-02-19 14:20:37 +0300

    new

--- src/Pages/Hot.jsx
+++ src/Pages/Hot.jsx
@@ -9,6 +9,13 @@ import espre from "../assets/images/espre.jpg"
 import HC from "../assets/images/Hc.jpg"
 import green from "../assets/images/green.jpg"
 import Emc from "../assets/images/Emc.jpg"
+import karak from "../assets/images/karak.jpg"
+import masala from "../assets/images/masala.jpg"
+import qah from "../assets/images/qah.jpg"
+import som from "../assets/images/som.jpg"
+import camel from "../assets/images/camel.jpg"
+import gtea from "../assets/images/gtea.jpg"
+import nom from "../assets/images/nom.jpg"
 function Hot (){
     return < div className="px-24 pt-14">
           <div className="grid justify-center">
@@ -88,6 +95,48 @@ function Hot (){
     <div className="mt-24">
         <h1 className="text-4xl text-center font-bold david-libre-font">Tea</h1>
         <div className="mt-20 lg:grid lg:grid-cols-5 gap-14 md:grid md:grid-cols-3 grid grid-cols-1">
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={masala} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">Masala Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Masala tea is spicy, warm, and aromatic.!</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={karak} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">karak Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Karak tea is bold, creamy, and spiced.</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={qah} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">kahwa<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Kahwa is fragrant, warm, and soothing.</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={som} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">Somali Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Somali tea is spiced, milky, and comforting.</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={camel} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">Camel Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Camel tea is rich, creamy, and nutritious.</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={nom} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Tea is warm, soothing, and refreshing.</p>
+            
+                        </div>
+              <div className="h-[300px] p-2  w-[200px]">
+                            <img className="h-[200px] rounded-md w-[170px]  "  src={gtea} alt="" />
+                            <h6 className="text-[#101A24] mt-2 text-[16px] font-bold">Green Tea<span className="text-[15px] text-[#5C6574] font-medium " >/ $1</span></h6>
+                            <p className="text-[#5C6574]">Green tea is light, fresh, and revitalizing.</p>
+            
+                        </div>
 
         </div>
     </div>
